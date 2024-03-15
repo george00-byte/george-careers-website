a0bed214d595981c1dd7994de6e17217c6eb9e5c
George Odhiambo <84243038+george00-byte@users.noreply.github.com>
2024-03-15 10:47:21 +0000

    Added the jobs api with jsonify

--- app.py
+++ app.py
@@ -29,10 +29,11 @@ JOBS = [{
 def hello_world():
   return render_template('home.html', jobs=JOBS)
 
+
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
-  
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
